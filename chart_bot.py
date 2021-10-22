ac8bb7378815b80315612f3cb59d519a1469fb8e
ilsang1212 <58781790+ilsang1212@users.noreply.github.com>
2021-10-23 07:05:22 +0900

    Update chart_bot.py

--- chart_bot.py
+++ chart_bot.py
@@ -448,7 +448,7 @@ def show_kfi_chart(update, ctx):
     if not db_checker:
         return
 
-    data_checker, result_msg = draw_chart(data_db, user_name, ["klay", "kfi", "ksta], interval_str, ratio_chart=True, ratio_list=[["klay", "kfi"], ["kfi", "ksta"]])
+    data_checker, result_msg = draw_chart(data_db, user_name, ["klay", "kfi", "ksta"], interval_str, ratio_chart=True, ratio_list=[["klay", "kfi"], ["kfi", "ksta"]])
 
     if not data_checker:
         ctx.bot.send_message(chat_id=update.message.chat_id, text=result_msg)
