402700ab6f27f2f28f13686d2a952794a8cc0dae
ilsang1212 <58781790+ilsang1212@users.noreply.github.com>
2021-11-25 14:36:55 +0900

    Update chart_bot.py

--- chart_bot.py
+++ chart_bot.py
@@ -421,7 +421,7 @@ def show_skai_chart(update, ctx):
     if not db_checker:
         return
 
-    data_checker, result_msg = draw_chart(data_db, user_name, ["skai", "vkai", "kai"], interval_str)
+    data_checker, result_msg = draw_chart(data_db, user_name, ["skai", "vkai", "akai", "kai"], interval_str)
 
     if not data_checker:
         ctx.bot.send_message(chat_id=update.message.chat_id, text=result_msg)
